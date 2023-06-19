b38c2ad7bf17f5507a60ce123f9ec2c3c8f042e7
Faramarz Farhangian <faramarz.farhangian.1@ens.etsmtl.ca>
2023-06-19 18:37:58 -0400

    Update BERT.py

--- Feature_extraction/BERT.py
+++ Feature_extraction/BERT.py
@@ -1,1 +1,14 @@
-
+from numpy import savez_compressed
+from numpy import asarray
+from numpy import load
+import joblib
+import torch
+import numpy as np
+from transformers import BertModel, BertTokenizer
+from transformers import DistilBertModel, DistilBertTokenizer
+from transformers import AlbertModel, AlbertTokenizer
+from transformers import BartModel, BartTokenizer
+from transformers import RobertaModel, RobertaTokenizer
+from transformers import ElectraModel, ElectraTokenizer
+from transformers import XLNetModel, XLNetTokenizer
+from allennlp.commands.elmo import ElmoEmbedder
